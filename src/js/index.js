fca764445040cbc280ec03ee12c69b4c7d6dc52a
John J Czaplewski <john@czaplewski.org>
2019-01-02 13:51:40 -0600

    Clean up entry point

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,3 @@
-import mapboxgl from './mapbox-gl-js'
 import OfflineManager from './offline-manager/OfflineManager'
-//import { setType, getType } from 'mapbox-gl/src/source/source'
-import OfflineRasterTileSource from './source/offline_raster_test'
 
-// Add a utility for managing offline tile caches
-mapboxgl.OfflineManager = OfflineManager
-
-// Add offline raster tile sources
-// mapboxgl.addSourceType('offline-raster', OfflineRasterTileSource)
-
-export default mapboxgl
+export default OfflineManager
